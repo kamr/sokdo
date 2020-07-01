9e63de35b3f360f29515201852605e859133f0bc
Kamran Khan <kkhan@palantir.com>
2020-07-01 13:19:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Client } from 'boardgame.io/react';
+import { TicTacToe } from './Game';
+import { TicTacToeBoard } from './Board';
+import { SocketIO } from 'boardgame.io/multiplayer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: 'localhost:8000' }),
+});
+
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <TicTacToeClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/lobby.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Lobby } from 'boardgame.io/react';
+import { TicTacToeBoard } from './board';
+import { TicTacToe } from './game';
+
+const server = `https://${window.location.hostname}`;
+const importedGames = [{ game: TicTacToe, board: TicTacToeBoard }];
+
+export default () => (
+  <div>
+    <h1>Lobby</h1>
+    <Lobby gameServer={server} lobbyServer={server} gameComponents={importedGames} />
+  </div>
+);
