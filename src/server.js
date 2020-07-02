c65c7d4b3aa31c33a4b3f505a671dcd5ee71895b
Kamran Khan <kkhan@palantir.com>
2020-07-02 16:41:18 -0400

    kao for heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import { Server } from 'boardgame.io/server';
 import path from 'path';
+import Koa from 'koa';
 import serve from 'koa-static';
+import mount from 'koa-mount';
 import { TicTacToe } from './game';
 
 const server = Server({ games: [TicTacToe] });
@@ -8,8 +10,11 @@ const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, './build');
-server.app.use(serve(frontEndAppBuildPath))
 
+// Serve the build directory
+const static_pages = new Koa();
+static_pages.use(serve(frontEndAppBuildPath));
+server.app.use(mount('/', static_pages));
 server.run(PORT, () => {
   server.app.use(
     async (ctx, next) => await serve(frontEndAppBuildPath)(
