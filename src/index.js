39c7d83c7970a3f92a5ef4e0eb69a6fb99cce1c6
Kamran Khan <kkhan@palantir.com>
2020-07-02 21:49:25 -0400

    trying to add lobby

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GameLobby from './GameLobby';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GameLobby />
   </React.StrictMode>,
   document.getElementById('root')
 );
