3829afeb9e0880e5e7cfb01b46b202a7e44bf737
Kamran Khan <kamranckhan@gmail.com>
2020-07-31 20:28:33 -0400

    try to run on 1 port for heroku

--- src/server.js
+++ src/server.js
@@ -1,38 +1,61 @@
+// server.js
+
 import { Server } from 'boardgame.io/server';
 import path from 'path';
-import Koa from 'koa';
 import serve from 'koa-static';
-import mount from 'koa-mount';
 import { TicTacToe } from './game';
-import fs from 'fs';
 
-// const server =  Server({ games: [TicTacToe] });
-// const PORT = process.env.PORT || 8000;
+const server = Server({ games: [TicTacToe] });
+const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
-// const frontEndAppBuildPath = path.resolve(__dirname, '../build');
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath))
 
-// Serve the build directory
-// const static_pages = new Koa();
-// static_pages.use(serve(frontEndAppBuildPath));
-// server.app.use(mount('/', static_pages));
-// server.run(PORT, () => {
-//   server.app.use(
-//     async (ctx, next) => await serve(frontEndAppBuildPath)(
-//       Object.assign(ctx, { path: 'index.html' }),
-//       next
-//     )
-//   )
-// });
-
-const PORT = process.env.PORT || 8000;
-const server = Server({
-  games: [TicTacToe],
-  // https: {
-  //   cert: fs.readFileSync('src/cert.pem'),
-  //   key: fs.readFileSync('src/key.pem'),
-  // }
-});
 server.run(PORT, () => {
-  console.log(`Serving at: http://localhost:${PORT}/`);
+  server.app.use(
+    async (ctx, next) => await serve(frontEndAppBuildPath)(
+      Object.assign(ctx, { path: 'index.html' }),
+      next
+    )
+  )
 });
+
+// import { Server } from 'boardgame.io/server';
+// import path from 'path';
+// import Koa from 'koa';
+// import serve from 'koa-static';
+// import mount from 'koa-mount';
+// import { TicTacToe } from './game';
+// import fs from 'fs';
+
+// // const server =  Server({ games: [TicTacToe] });
+// // const PORT = process.env.PORT || 8000;
+
+// // Build path relative to the server.js file
+// // const frontEndAppBuildPath = path.resolve(__dirname, '../build');
+
+// // Serve the build directory
+// // const static_pages = new Koa();
+// // static_pages.use(serve(frontEndAppBuildPath));
+// // server.app.use(mount('/', static_pages));
+// // server.run(PORT, () => {
+// //   server.app.use(
+// //     async (ctx, next) => await serve(frontEndAppBuildPath)(
+// //       Object.assign(ctx, { path: 'index.html' }),
+// //       next
+// //     )
+// //   )
+// // });
+
+// const PORT = process.env.PORT || 8000;
+// const server = Server({
+//   games: [TicTacToe],
+//   // https: {
+//   //   cert: fs.readFileSync('src/cert.pem'),
+//   //   key: fs.readFileSync('src/key.pem'),
+//   // }
+// });
+// server.run(PORT, () => {
+//   console.log(`Serving at: http://localhost:${PORT}/`);
+// });
